41870101c455169bff31ea509f26f5444763a4f7
alexlisson <63406187+alexlisson@users.noreply.github.com>
2020-04-19 22:59:38 +0300

    Updated with SCM and dist repo - and fix maven

--- src/test/java/com/example/devops/web/WelcomeControllerTest.java
+++ src/test/java/com/example/devops/web/WelcomeControllerTest.java
@@ -7,16 +7,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+//@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 @WebAppConfiguration
 class WelcomeControllerTest {
